550a2452a003bed453721daa3fc608a008b34a56
ADigitalNative <adigitalnative@gmail.com>
2012-06-04 17:06:00 -0400

    Rails comes in, rails goes out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drawbridge::Application.config.secret_token = 'c1b11442ada54a44488a542502a49698b409c25a27d8521172993a1065d27d3b3ce40a8c6b309e0efe6cc555e340eeb62ebdeeea3cd032c7429c9b153189af27'
