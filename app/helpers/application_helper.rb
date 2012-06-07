d93ce47ff78b5eabb0961323686a7bc2d68a9191
ADigitalNative <adigitalnative@gmail.com>
2012-06-07 16:18:05 -0400

    Adding mockup current chatrooms to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
     end
   end
 
+  def master_room_list
+    chat_client = Faraday.new('http://localhost:3000/api/v1/')
+    responses = JSON.parse(chat_client.get('chat_rooms').body)
+    @chat_rooms = []
+    responses.each do |response|
+      @chat_rooms << {id: response["chat_room"]["id"], title: response["chat_room"]["title"]}
+    end
+    @chat_rooms
+  end
+
+
+    # responses = JSON.parse(@chat_client.get('chat_rooms').body)
+    # @chat_rooms = []
+    # responses.each do |response|
+    #   @chat_rooms << {id: response["chat_room"]["id"], title: response["chat_room"]["title"]}
+    # end
+    # @chat_rooms
+
 end
