d1a32a31399d248805a1fa9d4b82a9261e37830b
ADigitalNative <adigitalnative@gmail.com>
2012-06-11 10:17:19 -0400

    New Messages use partial, paving way for Faye adding messages

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -12,28 +12,25 @@ class ChatRoomsController < ApplicationController
     redirect_to chat_rooms_path
   end
 
-  # def show
-  #   # JQTODO: Use gem for this!
-  #   #   Question is, how do I get all messages ass. with a particular room ID?
-  #   # @chat_room = ChatRoom.find_by_id(params[:id])
-  #   @messages = Message.all
-  #   raise @messages.inspect
-  # end
-
   def show
-    build_chat_client
-    response = @chat_client.get("chat_rooms/#{params[:id]}")
-    response = JSON.parse(response.body)
-    @response = response["chat_room"]
-    @messages = []
-    if @response["messages"]
-      @response["messages"].each do |message|
-        @messages << message["message"]["content"]
-      end
-    else
-      @messages = ["Empty Room"]
-    end
+    @chat_room = ChatRoom.find_by_id(params[:id])
+    @messages = @chat_room.messages
   end
+
+  # def show
+  #   build_chat_client
+  #   response = @chat_client.get("chat_rooms/#{params[:id]}")
+  #   response = JSON.parse(response.body)
+  #   @response = response["chat_room"]
+  #   @messages = []
+  #   if @response["messages"]
+  #     @response["messages"].each do |message|
+  #       @messages << message["message"]["content"]
+  #     end
+  #   else
+  #     @messages = ["Empty Room"]
+  #   end
+  # end
 
   private
 
